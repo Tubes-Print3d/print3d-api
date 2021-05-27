9e8f4b027892176cacff3e914e355c427c309fe3
AtallaSatrio <senpoki@student.telkomuniversity.ac.id>
2021-05-27 10:35:47 +0700

    refactor : edit and delete product

--- validators/product.validator.js
+++ validators/product.validator.js
@@ -1,10 +1,30 @@
-const { body } = require("express-validator");
+const { body, param, Result } = require("express-validator");
+const Produk = require("../models/produk.model");
 const { wrap } = require("../utils/validator");
 
 const addProduct = [
   body("nama").isString().exists(),
-  body("royalty").isFloat() ,
-  body("visibility").optional().isBoolean()
+  body("royalty").isFloat(),
+  body("visibility").optional().isBoolean(),
 ];
 
-module.exports = wrap({ addProduct });
+const editProduct = [
+  param("id").custom((id, { req }) => {
+    return Produk.exists({ _id: id }).then((exists) => {
+      if (!exists) {
+        return Promise.reject("Cannot find product");
+      }
+    });
+  }),
+];
+
+const deleteProduct = [
+  param("id").custom((id, { req }) => {
+    return Produk.exists({ _id: id }).then((exists) => {
+      if (!exists) {
+        return Promise.reject("Cannot find product");
+      }
+    });
+  }),
+];
+module.exports = wrap({ addProduct, editProduct, deleteProduct });
